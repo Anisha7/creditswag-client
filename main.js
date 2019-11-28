63bec3144ac52ea135a42b50161bb281187d40ea
Anisha Jain <anishajain@outlook.com>
2019-11-28 00:46:14 -0800

    code style

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import LoginScreen from './screens/Login/';
-import SignupScreen from './screens/Signup/';
+import LoginScreen from './screens/Login';
+import SignupScreen from './screens/Signup';
 
 // FUTURE: create Tab navigator for in-app tabs/pages
 // https://stackoverflow.com/questions/54210517/react-native-set-homepage-screen-main-activity
