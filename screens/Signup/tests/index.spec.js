c3d0e76fa150372720c5703f8892801e8fbe9373
Anisha Jain <anishajain@outlook.com>
2019-12-02 23:46:30 -0800

    signup tests

---
+++ screens/Signup/tests/index.spec.js
@@ -0,0 +1,56 @@
+import expect from 'expect'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+
+import SignupScreen from '../';
+import {
+    Text,
+    TextInput,
+    TouchableWithoutFeedback,
+    TouchableHighlight,
+    View
+  } from "react-native";
+
+
+describe('<SignupScreen />', () => {
+    it('has style and children props', () => {
+      const Wrapper = shallow(
+          <SignupScreen />);
+      const props = Wrapper.props()
+      expect(Object.keys(props)).toStrictEqual([ 'style', 'children' ])
+      expect(props.style).toBeDefined()
+      expect(props.children).toBeDefined()
+    });
+  
+    it('renders', () => {
+      const Wrapper = shallow(
+          <SignupScreen />);
+      expect(Wrapper.find(TextInput)).toHaveLength(4);
+      expect(Wrapper.find(TouchableWithoutFeedback)).toHaveLength(2);
+      expect(Wrapper.find(TouchableHighlight)).toHaveLength(1);
+      expect(Wrapper.find(Text)).toHaveLength(3);
+      expect(Wrapper.find(View)).toHaveLength(3);
+    });
+
+    it('signs up user with credit swag account', () => {
+        // TODO
+        expect(true).toBe(true);
+    })
+
+    it('prompts user to login if email is already in the system', () => {
+        // TODO
+        expect(true).toBe(true);
+    })
+
+    it('logs in user with google', () => {
+        // TODO
+        expect(true).toBe(true);
+    })
+
+    it('logs in user with facebook', () => {
+        // TODO
+        expect(true).toBe(true);
+    })
+    
+  });
