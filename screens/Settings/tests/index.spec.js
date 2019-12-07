aaa14453e549a52caec69eedfa02d622ecdc8f64
Anisha Jain <anishajain@outlook.com>
2019-12-07 02:27:55 -0800

    settings tests

--- screens/Settings/tests/index.spec.js
+++ screens/Settings/tests/index.spec.js
@@ -14,37 +14,15 @@ import SettingsScreen, {
 } from "../";
 import {
   Text,
-  TextInput,
-  TouchableWithoutFeedback,
+  ScrollView,
   TouchableHighlight,
   View,
   Image
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import ToggleSwitch from "toggle-switch-react-native";
 
 describe("<ProfileTab />", () => {
-  it("has props", () => {
-      const img = "https://via.placeholder.com/80"
-    const Wrapper = shallow(
-      <ProfileTab
-        img={img}
-        name={"Timofey Makhlay"}
-      />
-    );
-    const props = Wrapper.props();
-    expect(Object.keys(props)).toStrictEqual([
-      "style",
-      "children"
-    ]);
-    expect(props.style).toBeDefined();
-    expect(props.children).toBeDefined();
-    // TODO: can we do this with function components?
-    // expect(props.img).toBeDefined();
-    // expect(props.img).toBe("https://via.placeholder.com/80");
-    // expect(props.name).toBeDefined();
-    // expect(props.img).toBe("Timofey Makhlay");
-  });
-
   it("renders", () => {
     const Wrapper = shallow(
       <ProfileTab
@@ -60,19 +38,6 @@ describe("<ProfileTab />", () => {
 });
 
 describe("<AccountSettingsTab />", () => {
-  it("has style and children props", () => {
-    const Wrapper = shallow(<AccountSettingsTab itle={"Add re-occuring bill"} link={"bill"} />);
-    const props = Wrapper.props();
-    expect(Object.keys(props)).toStrictEqual([
-      "style",
-      "children",
-    ]);
-    expect(props.style).toBeDefined();
-    expect(props.children).toBeDefined();
-    // expect(props.title).toBeDefined();
-    // expect(props.link).toBeDefined();
-  });
-
   it("renders", () => {
     const Wrapper = shallow(<AccountSettingsTab title={"Add re-occuring bill"} link={"bill"} />);
     expect(Wrapper.find(TouchableHighlight)).toHaveLength(1);
@@ -82,40 +47,34 @@ describe("<AccountSettingsTab />", () => {
   });
 });
 
-// describe("<NotificationSettingsTab />", () => {
-//   it("has style and children props", () => {
-//     const Wrapper = shallow(<NotificationSettingsTab title={} />);
-//     const props = Wrapper.props();
-//     expect(Object.keys(props)).toStrictEqual(["style", "children"]);
-//     expect(props.style).toBeDefined();
-//     expect(props.children).toBeDefined();
-//   });
+describe("<NotificationSettingsTab />", () => {
 
-//   it("renders", () => {
-//     const Wrapper = shallow(<NotificationSettingsTab title={} />);
-//     // expect(Wrapper.find(TextInput)).toHaveLength(2);
-//     // expect(Wrapper.find(TouchableWithoutFeedback)).toHaveLength(2);
-//     // expect(Wrapper.find(TouchableHighlight)).toHaveLength(4);
-//     // expect(Wrapper.find(Text)).toHaveLength(4);
-//     // expect(Wrapper.find(View)).toHaveLength(5);
-//   });
-// });
+  it("renders", () => {
+    const Wrapper = shallow(<NotificationSettingsTab title={"thing"} />);
+    expect(Wrapper.find(ToggleSwitch)).toHaveLength(1);
+    expect(Wrapper.find(Text)).toHaveLength(1);
+    expect(Wrapper.find(View)).toHaveLength(1);
+  });
+});
 
-// describe("<SettingsScreen />", () => {
-//   it("has style and children props", () => {
-//     const Wrapper = shallow(<SettingsScreen />);
-//     const props = Wrapper.props();
-//     expect(Object.keys(props)).toStrictEqual(["style", "children"]);
-//     expect(props.style).toBeDefined();
-//     expect(props.children).toBeDefined();
-//   });
+describe("<SettingsScreen />", () => {
+  it("has style and children props", () => {
+    const Wrapper = shallow(<SettingsScreen />);
+    const props = Wrapper.props();
+    expect(Object.keys(props)).toStrictEqual(["style", "children"]);
+    expect(props.style).toBeDefined();
+    expect(props.children).toBeDefined();
+  });
 
-//   it("renders", () => {
-//     const Wrapper = shallow(<SettingsScreen />);
-//     // expect(Wrapper.find(TextInput)).toHaveLength(2);
-//     // expect(Wrapper.find(TouchableWithoutFeedback)).toHaveLength(2);
-//     // expect(Wrapper.find(TouchableHighlight)).toHaveLength(4);
-//     // expect(Wrapper.find(Text)).toHaveLength(4);
-//     // expect(Wrapper.find(View)).toHaveLength(5);
-//   });
-// });
+  it("renders", () => {
+    const Wrapper = shallow(<SettingsScreen />);
+    expect(Wrapper.find(Image)).toHaveLength(2);
+    expect(Wrapper.find(ScrollView)).toHaveLength(1);
+    expect(Wrapper.find(TouchableHighlight)).toHaveLength(1);
+    expect(Wrapper.find(Text)).toHaveLength(4);
+    expect(Wrapper.find(View)).toHaveLength(5);
+    expect(Wrapper.find(ProfileTab)).toHaveLength(1);
+    expect(Wrapper.find(AccountSettingsTab)).toHaveLength(4);
+    expect(Wrapper.find(NotificationSettingsTab)).toHaveLength(6);
+  });
+});
